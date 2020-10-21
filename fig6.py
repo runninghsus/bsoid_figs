905c97748bacd2609c04dbe1f879ec8a45dc7eaa
Alex <alexander.hsu.han@gmail.com>
2020-10-21 14:23:24 -0400

    update fig6.py

--- fig6.py
+++ fig6.py
@@ -17,10 +17,33 @@ print('\n' * 1)
 variables = str(['c_hg_len_N4_1', 'a_hg_len_N4_1'])
 c = str(['deepskyblue', 'red'])
 x_range = str([0, 2])
+leg = '1'
 
 p = subprocess.Popen([sys.executable, './subroutines/kinematics_cdf.py',
                       '-p', path, '-v', variables,
-                      '-c', c, '-r', x_range, '-m', fig_format, '-o', outpath])
+                      '-c', c, '-r', x_range, '-l', leg, '-m', fig_format, '-o', outpath])
+p.communicate()
+p.kill()
+
+
+variables = str(['c_hg_speed_N4_1', 'a_hg_speed_N4_1'])
+x_range = str([0, 10])
+leg = '0'
+
+p = subprocess.Popen([sys.executable, './subroutines/kinematics_cdf.py',
+                      '-p', path, '-v', variables,
+                      '-c', c, '-r', x_range, '-l', leg, '-m', fig_format, '-o', outpath])
+p.communicate()
+p.kill()
+
+
+variables = str(['c_hg_dur_N4', 'a_hg_dur_N4'])
+x_range = str([0, 4])
+
+
+p = subprocess.Popen([sys.executable, './subroutines/kinematics_cdf.py',
+                      '-p', path, '-v', variables,
+                      '-c', c, '-r', x_range, '-l', leg, '-m', fig_format, '-o', outpath])
 p.communicate()
 p.kill()
 
@@ -32,26 +55,48 @@ print('\n' * 1)
 variables = str(['c_fg_len_N4_1', 'a_fg_len_N4_1'])
 c = str(['deepskyblue', 'red'])
 x_range = str([0, 2])
+leg = '1'
 
 p = subprocess.Popen([sys.executable, './subroutines/kinematics_cdf.py',
                       '-p', path, '-v', variables,
-                      '-c', c, '-r', x_range, '-m', fig_format, '-o', outpath])
+                      '-c', c, '-r', x_range, '-l', leg, '-m', fig_format, '-o', outpath])
 p.communicate()
 p.kill()
 
 
-# FIG6F
-print('\n' * 1)
-print('Preparing itch kinematics cdf curves as xxx.{} found in fig6f...'.format(fig_format))
-print('\n' * 1)
-variables = str(['c_it_len_N4_1', 'a_it_len_N4_1'])
-c = str(['deepskyblue', 'red'])
-x_range = str([1, 5])
+variables = str(['c_fg_speed_N4_1', 'a_fg_speed_N4_1'])
+x_range = str([0, 10])
+leg = '0'
 
 p = subprocess.Popen([sys.executable, './subroutines/kinematics_cdf.py',
                       '-p', path, '-v', variables,
-                      '-c', c, '-r', x_range, '-m', fig_format, '-o', outpath])
+                      '-c', c, '-r', x_range, '-l', leg, '-m', fig_format, '-o', outpath])
 p.communicate()
 p.kill()
-print("All xxx.{}s generated. see {} for results".format(fig_format, outpath))
-print('-' * 50)
+
+
+variables = str(['c_fg_dur_N4', 'a_fg_dur_N4'])
+x_range = str([0, 4])
+
+p = subprocess.Popen([sys.executable, './subroutines/kinematics_cdf.py',
+                      '-p', path, '-v', variables,
+                      '-c', c, '-r', x_range, '-l', leg, '-m', fig_format, '-o', outpath])
+p.communicate()
+p.kill()
+
+
+# # FIG6F
+# print('\n' * 1)
+# print('Preparing itch kinematics cdf curves as xxx.{} found in fig6f...'.format(fig_format))
+# print('\n' * 1)
+# variables = str(['c_it_len_N4_1', 'a_it_len_N4_1'])
+# c = str(['deepskyblue', 'red'])
+# x_range = str([1, 5])
+#
+# p = subprocess.Popen([sys.executable, './subroutines/kinematics_cdf.py',
+#                       '-p', path, '-v', variables,
+#                       '-c', c, '-r', x_range, '-m', fig_format, '-o', outpath])
+# p.communicate()
+# p.kill()
+# print("All xxx.{}s generated. see {} for results".format(fig_format, outpath))
+# print('-' * 50)
--- subroutines/kinematics_cdf.py
+++ subroutines/kinematics_cdf.py
@@ -6,12 +6,12 @@ import sys, getopt
 from ast import literal_eval
 
 
-def plot_cdf(var, data, c, x_range, fig_size, fig_format, outpath):
+def plot_cdf(var, data, c, x_range, bn, tk, leg, fig_size, fig_format, outpath):
     figure(num=None, figsize=fig_size, dpi=300, facecolor='w', edgecolor='k')
     ax = plt.axes()
-    values1, base = np.histogram(data[0], bins=np.arange(0, x_range[1], 0.2),
+    values1, base = np.histogram(data[0], bins=np.arange(0, x_range[1]+0.5, bn),
                                  weights=np.ones(len(data[0])) / len(data[0]), density=False)
-    values2, base = np.histogram(data[1], bins=np.arange(0, x_range[1], 0.2),
+    values2, base = np.histogram(data[1], bins=np.arange(0, x_range[1]+0.5, bn),
                                  weights=np.ones(len(data[1])) / len(data[1]), density=False)
     values1 = np.append(values1, 0)
     values2 = np.append(values2, 0)
@@ -27,7 +27,11 @@ def plot_cdf(var, data, c, x_range, fig_size, fig_format, outpath):
     ax.set_ylim(0, 1)
     ax.set_axisbelow(True)
     ax.grid(linestyle='-', linewidth=5, axis='both')
-    ax.set_xticks(np.arange(x_range[0], x_range[1]+0.1, (x_range[1]-x_range[0])/4))
+    ax.set_xticks(np.arange(x_range[0], x_range[1]+0.1, (x_range[1]-x_range[0])/tk))
+    if leg:
+        lgnd = plt.legend(loc=0, prop={'family': 'Helvetica', 'size': 60})
+        lgnd.legendHandles[0]._legmarker.set_markersize(8)
+        lgnd.legendHandles[1]._legmarker.set_markersize(8)
     ax.set_yticks(np.arange(0, 1.1, 0.2))
     ax.xaxis.set_ticklabels([])
     ax.yaxis.set_ticklabels([])
@@ -53,12 +57,13 @@ def main(argv):
     var = None
     c = None
     x_range = None
+    leg = None
     fig_format = None
     outpath = None
     options, args = getopt.getopt(
         argv[1:],
-        'p:v:c:r:m:o:',
-        ['path=', 'variables=', 'colors=', 'range=', 'format=', 'outpath='])
+        'p:v:c:r:l:m:o:',
+        ['path=', 'variables=', 'colors=', 'range=', 'legend=', 'format=', 'outpath='])
 
     for option_key, option_value in options:
         if option_key in ('-p', '--path'):
@@ -69,6 +74,8 @@ def main(argv):
             c = option_value
         elif option_key in ('-r', '--range'):
             x_range = option_value
+        elif option_key in ('-l', '--legend'):
+            leg = option_value
         elif option_key in ('-m', '--format'):
             fig_format = option_value
         elif option_key in ('-o', '--outpath'):
@@ -84,7 +91,8 @@ def main(argv):
     print('Plotting...')
     mat = load_mat(path)
     data = [mat[literal_eval(var)[0]][0], mat[literal_eval(var)[1]][0]]
-    plot_cdf(literal_eval(var), data, literal_eval(c), literal_eval(x_range), (16, 12), fig_format, outpath)
+    plot_cdf(literal_eval(var), data, literal_eval(c), literal_eval(x_range),
+             0.01, 4, int(leg), (16, 16), fig_format, outpath)
 
 
 if __name__ == '__main__':
